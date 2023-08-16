dcad82bbfb14aca9be603284cd1647fb725ed31c
DESKTOP-SAWA\ASUS <eng.baraahmassri@gmail.com>
2023-08-17 00:28:24 +0300

    phase-1-constructor-functions-lab

---
+++ index.js
@@ -0,0 +1,22 @@
+// Scooter constructor function
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+
+// Driver constructor function
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+// PickupLocation constructor function
+function PickupLocation(address, city) {
+  this.address = address;
+  this.city = city;
+}
+const myScooter = new Scooter(2022, "Red", "Model X");
+const myDriver = new Driver("John Doe", 30, 5);
+const myLocation = new PickupLocation("123 Main St", "New York");
